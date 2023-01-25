e8afbb0e04e03abf8a222140eb2c36941904eddb
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2023-01-25 16:05:35 -0300

    Security Group

---
+++ sg/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0b5eea76982371e91"
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.webtraffic.name]
+}
+
+resource "aws_security_group" "webtraffic" {
+  name = "Allow HTTPS"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
